67b5439d8ce9c0ccb270a26c74a6ed243e6cc176
Ivan <elderivanargueta04@gmail.com>
2026-03-29 13:06:34 -0700

    Issues fixed
    Wasn't sending and reading from wire correctly because of the baud rate. It was too fast, lowered it and works fine

--- UARTSim.cpp
+++ UARTSim.cpp
@@ -5,19 +5,21 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #include <thread>
-#include <chrono>
 
-uint8_t BUFFER_SIZE = 26;
+constexpr uint8_t BUFFER_SIZE = 26;
+constexpr uint8_t BIT_ARRAY_SIZE = 10;
+
 constexpr uint16_t BAUD_RATE = 9600;
-double BIT_PERIOD = 1.0 / BAUD_RATE;
+double BIT_PERIOD = 0.1;
 double MID_POINT = BIT_PERIOD / 2;
 
 uint8_t *buffer = new uint8_t[BUFFER_SIZE]{0};
 uint8_t head = 0;
 uint8_t tail = 0;
-uint8_t bitArray[10];
-uint8_t wire = 1;
 uint8_t count = 0;
+
+uint8_t bitArray[BIT_ARRAY_SIZE];
+std::atomic<uint8_t> wire = 1;
 
 
 bool write(uint8_t value) {
@@ -60,32 +62,26 @@ void STX(const uint8_t value) {
         bitArray[i+1] = ((value >> i) & 00000001); // right shift value by i times, do & comparison with 1
     }
 
-
+    // looping through bitArray with buffers already set
     for (uint8_t i = 0; i < 10; i++) {
         wire = bitArray[i];
         std:: cout << "Wire: " << static_cast<int>(wire) << std:: endl;
-        std:: this_thread::sleep_for(std::chrono::duration<double>(BIT_PERIOD));
+        std::this_thread::sleep_for(std::chrono::duration<double>(BIT_PERIOD));
     }
 }
 
 void SRX() {
     while (wire == 1) {
-        wire = 0;
         std:: cout << "Waiting on init 0 bit" << std:: endl;
     }
     std:: cout << "Seen starting 0 bit: " << std:: endl;
-    std::this_thread::sleep_for(std::chrono::duration<double>(MID_POINT));
     uint8_t reassembledValue = 0;
-
+    std::this_thread::sleep_for(std::chrono::duration<double>(MID_POINT));
     for (int i = 0; i < 8; i++) {
         std::this_thread::sleep_for(std::chrono::duration<double>(BIT_PERIOD));
         reassembledValue = reassembledValue  | (wire << i);
     }
-    if (bitArray[9] != 1) {
-        std:: cout << "BIT ERROR, WRONG ENDING BIT" << std:: endl;
-        return;
-    }
-    wire = bitArray[9];
+
     write(reassembledValue);
 }
 
@@ -106,8 +102,16 @@ void SRX() {
 
 int main() {
 
-    STX(15);
-    SRX();
+    std::thread txThread(STX, 77);
+    std::thread rxThread(SRX);
+
+    txThread.join();
+    rxThread.join();
+
+    uint8_t readValue = 0;
+    while (read(readValue)) {
+        std::cout << "Buffer value: " << static_cast<int>(readValue) << std::endl;
+    }
 
     delete[] buffer;
     return 0;
