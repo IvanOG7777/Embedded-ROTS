71d5b00c17e0e017696a394475bca9042a02296e
Ivan <elderivanargueta04@gmail.com>
2026-03-28 19:06:36 -0700

    Working on RX
    It's taken me a while but im working on the RX function to read passed bit array and reassemble back into uint8_t as MSB. I was doing some reading and found out about baud rate, (rate a bit is sent/(read?) at a time). Im working on my ipad through the steps.

--- UARTSim.cpp
+++ UARTSim.cpp
@@ -7,6 +7,9 @@
 #include <bits/stdc++.h>
 
 uint8_t BUFFER_SIZE = 26;
+constexpr uint16_t BAUD_RATE = 9600;
+double BIT_PERIOD = 1.0 / BAUD_RATE;
+double MID_POINT = BIT_PERIOD / 2;
 
 uint8_t *buffer;
 uint8_t head = 0;
@@ -14,15 +17,6 @@ uint8_t tail = 0;
 uint8_t bitArray[10];
 
 uint8_t count = 0;
-
-bool RX(uint8_t value) {
-    if (count == BUFFER_SIZE) return false;
-
-    std:: bitset<8> bitValue(value);
-    bitValue.flip();
-
-    return true;
-}
 
 //transmitter
 uint8_t* TX(uint8_t value) {
@@ -35,17 +29,31 @@ uint8_t* TX(uint8_t value) {
 }
 
 
+bool RX(uint8_t *bitArray) {
+    if (count == BUFFER_SIZE) return false; // if buffer is already full reject
+    if (bitArray[0] != 0) return false; // makes use the start signal is 0
+    uint8_t reassembledValue = 0;
 
+    for (int i = 1; i < 8; i++) {
+        reassembledValue = ((reassembledValue << i) | bitArray[i]);
+    }
+    std:: cout << "Value within RX: " << static_cast<int>(reassembledValue) << std:: endl;
+
+    return true;
+}
 
 
 int main() {
 
+    std:: cout << BIT_PERIOD << std:: endl;
+    std:: cout << MID_POINT << std:: endl;
     uint8_t *array = TX(15);
     for (int i = 0; i < 10; i++) {
         std:: cout << static_cast<int>(array[i]);
     }
     std:: cout << std:: endl;
-    std:: cout << std::bitset<8>(15);
+    std:: cout << std::bitset<8>(15) << std:: endl;
+    RX(array);
     delete[] buffer;
     return 0;
 }
