0cfa553e33a1efa240304d4e1504b1cd9447fed2
Ivan <elderivanargueta04@gmail.com>
2026-03-30 11:12:56 -0700

    State machine

---
+++ stateMachineLightController.cpp
@@ -0,0 +1,23 @@
+//
+// Created by elder on 3/30/2026.
+//
+
+#include <iostream>
+
+enum LIGHTS {
+    GREEN = 0,
+    YELLOW,
+    RED,
+};
+
+enum TICK_TIMES {
+    GREEN_TICK = 30,
+    YELLOW_TICK = 5,
+    RED_TICK = 10,
+    RED_WALK = 20,
+};
+
+int main() {
+
+    return 0;
+}
